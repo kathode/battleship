fe436e28d724b39141d89ba1c4da84537a85d70a
Kathode <work.qiyan@gmail.com>
2026-01-10 16:55:46 +1100

    Add ship class

---
+++ src/classes/ship.js
@@ -0,0 +1,51 @@
+export class Ship {
+  constructor(size, coordinates, orientation) {
+    this.size = size;
+    this.coordinates = coordinates;
+    this.orientation = orientation;
+    this.isSunk = false;
+    this.hitCount = 0;
+  }
+
+  hit(xPosition, yPosition) {
+    // hits origin
+    if (xPosition === this.coordinates.x && yPosition === this.coordinates.y) {
+      this.hitCount++;
+      return true;
+    }
+
+    // hits horizontal position other than origin
+    if (
+      this.orientation === "horizontal" &&
+      yPosition === this.coordinates.y &&
+      xPosition > this.coordinates.x &&
+      xPosition <= this.coordinates.x + this.size - 1
+    ) {
+      this.hitCount++;
+      return true;
+    }
+
+    // hits vertical position other than origin
+    if (
+      this.orientation === "vertical" &&
+      xPosition === this.coordinates.x &&
+      yPosition > this.coordinates.y &&
+      yPosition <= this.coordinates.y + this.size - 1
+    ) {
+      this.hitCount++;
+      return true;
+    }
+
+    return false;
+  }
+
+  isSinking() {
+    if (this.size === this.hitCount) {
+      this.isSunk = true;
+
+      return true;
+    }
+
+    return false;
+  }
+}
